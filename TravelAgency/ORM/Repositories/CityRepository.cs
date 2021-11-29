ade1dfd2f1a1355c2109a796cbad700af36e620a
TheEvilShot <samofalov.19982805@gmail.com>
2021-11-29 16:01:45 +0300

    Добавлены репозитории на все основные сущности

---
+++ TravelAgency/ORM/Repositories/CityRepository.cs
@@ -0,0 +1,54 @@
+﻿using Domain;
+using ORM.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORM.Repositories
+{
+    class CityRepository:IRepository<City>
+    {
+        private TourContext _context;
+        public CityRepository(TourContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+        public City Create(City city)
+        {
+            this._context.City.Add(city);
+            this._context.SaveChanges();
+            return city;
+        }
+
+        public void Delete(int id)
+        {
+            if (!this.TryGet(id, out var city))
+            {
+                return;
+            }
+            this._context.City.Remove(city);
+            this._context.SaveChanges();
+        }
+
+        public IQueryable<City> Filter(Expression<Func<City, bool>> filter)
+        {
+            return this.GetAll().Where(filter);
+        }
+
+        public IQueryable<City> GetAll() => _context.City;
+
+        public City GetById(int id)
+        {
+            return this.GetAll().SingleOrDefault(t => t.Id == id);
+        }
+
+        public bool TryGet(int id, out City city)
+        {
+            city = this.GetAll().SingleOrDefault(t => t.Id == id);
+            return city != null;
+        }
+    }
+}
